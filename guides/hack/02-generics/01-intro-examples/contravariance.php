90b64afa153070f01d6449b64e2c77dd114bc32c
Rex Jaeschke <rex@RexJaeschke.com>
2015-10-15 11:28:15 -0400

    Update contravariance.php

--- guides/hack/02-generics/01-intro-examples/contravariance.php
+++ guides/hack/02-generics/01-intro-examples/contravariance.php
@@ -1,3 +1,19 @@
-
+<?hh
 
 namespace Hack\UserDocumentation\Generics\Variance\Examples\Contravariance;
+
+class C<-T> {
+  public function __construct(private T $t) {}
+}
+
+class Animal {}
+class Cat extends Animal {}
+
+function f(C<Cat> $p1): void { var_dump($p1); }
+
+function main(): void {
+  f(new C(new Animal()));	// accepted
+  f(new C(new Cat()));
+}
+
+main();
